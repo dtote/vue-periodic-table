94980763307eb2971107994c85d4164fc078d149
Juan Fran Martin <juanframaro@gmail.com>
2021-11-24 20:41:22 +0000

    added element and table as components

---
+++ src/components/Element.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="square">
+  <div>{{atomicNumber}}</div>
+  <div>{{symbol}}</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Element',
+  props: ['atomicNumber', 'symbol']
+}
+</script>
+
+<style scoped>
+.square{
+    border: 1px solid black;
+    height: 200px;
+    width: 200px;
+    background-color: pink;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="rectangle">
+      <Element v-for="element, index in periodicElements" :key="index" :atomicNumber="element.atomicNumber" :symbol="element.symbol"></Element>
+  </div>
+</template>
+
+<script>
+import * as periodicElements from '../assets/data.json'
+import Element from './Element.vue'
+export default {
+  name: 'Table',
+  components: {
+    Element
+  },
+  data: function () {
+    return {
+      periodicElements: periodicElements.default
+    }
+  },
+  methods: {
+    getData () {
+      console.log(periodicElements.default)
+    }
+  },
+  mounted () {
+    this.getData()
+  }
+}
+</script>
+
+<style>
+.rectangle {
+  border: 1px solid black;
+  display: flex;
+}
+</style>
